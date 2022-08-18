5cac9925d1be89d474a5f33a9bb6a793d5f81d8e
Alexandr <caiiia_podoplelov@mail.ru>
2022-08-18 14:52:51 +0300

    work with api

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,12 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import Header from '../components/Header';
+import requests from '../utils/requests';
 
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ netflixOriginals }) => {
+  console.log(netflixOriginals);
+
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
       <Head>
@@ -13,9 +16,46 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      <main></main>
+      <main>
+        <section></section>
+      </main>
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  };
 };
 
 
